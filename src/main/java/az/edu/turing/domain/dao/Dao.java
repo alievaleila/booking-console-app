fcc449c1b7ce8b20ad056d4228ac1f0b251b088a
CSAsim <asimpenahov6@gmail.com>
2024-11-11 11:50:30 +0400

    Dao updated

--- src/main/java/az/edu/turing/domain/dao/Dao.java
+++ src/main/java/az/edu/turing/domain/dao/Dao.java
@@ -3,10 +3,14 @@ package az.edu.turing.domain.dao;
 import java.util.Collection;
 import java.util.Optional;
 
-public interface Dao <E, T>{
+public interface Dao<E, T> {
     E create(E e);
+
     Collection<E> getAll();
+
     Optional<E> getById(T id);
+
     E deleteById(T id);
+
     E update(E e);
 }
