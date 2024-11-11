5fb0c4046df4e1c26a0a93a23b54c3f5474aab9c
Aliko313 <168551662+Aliko313@users.noreply.github.com>
2024-11-11 15:31:43 +0400

    FlightDaoInmemory

--- src/main/java/az/edu/turing/domain/dao/impl/memory/FlightDaoInmemory.java
+++ src/main/java/az/edu/turing/domain/dao/impl/memory/FlightDaoInmemory.java
@@ -4,31 +4,40 @@ import az.edu.turing.domain.dao.inter.FlightDao;
 import az.edu.turing.domain.entity.FlightEntity;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class FlightDaoInmemory extends FlightDao {
+    private final Map<String, FlightEntity> flightDatabase = new HashMap<>();
+
     @Override
     public FlightEntity create(FlightEntity flightEntity) {
-        return null;
+        flightDatabase.put(flightEntity.getId(), flightEntity);
+        return flightEntity;
     }
 
     @Override
     public Collection<FlightEntity> getAll() {
-        return null;
+        return flightDatabase.values();
     }
 
     @Override
     public Optional<FlightEntity> getById(String id) {
-        return Optional.empty();
+        return Optional.ofNullable(flightDatabase.get(id));
     }
 
     @Override
     public FlightEntity deleteById(String id) {
-        return null;
+        return flightDatabase.remove(id);
     }
 
     @Override
     public FlightEntity update(FlightEntity flightEntity) {
+        if (flightDatabase.containsKey(flightEntity.getId())) {
+            flightDatabase.put(flightEntity.getId(), flightEntity);
+            return flightEntity;
+        }
         return null;
     }
 }
